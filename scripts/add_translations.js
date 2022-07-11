bece06221b1095d5487f89b83a86463b6ba1f4fa
André Grillo <andregrillo@gmail.com>
2022-07-11 18:28:41 +0100

    Hook fixed

--- scripts/add_translations.js
+++ scripts/add_translations.js
@@ -25,7 +25,8 @@ module.exports = function(context) {
       }
     }*/
 
-    localizationStringsJSON = '{"languages":["pt","it", "en"], "translations": {"pt": {"config_ios": {"NSCameraUsageDescription": "Precisamos aceder a tua câmera!", "NSLocationWhenInUseUsageDescription": "Precisamos da tua localização!", "NSLocationAlwaysAndWhenInUseUsageDescription": "Precisamos da tua localização!", "NSLocationAlwaysUsageDescription": "Precisamos da tua localização!"}},"it": {"config_ios": {"NSCameraUsageDescription": "Dobbiamo accedere alla tua fotocamera!", "NSLocationWhenInUseUsageDescription": "Dobbiamo accedere alla tua posizione!", "NSLocationAlwaysAndWhenInUseUsageDescription": "Dobbiamo accedere alla tua posizione!", "NSLocationAlwaysUsageDescription": "Dobbiamo accedere alla tua posizione!"}},"en": {"config_ios": {"NSCameraUsageDescription": "We need to access your camera!", "NSLocationWhenInUseUsageDescription": "We need to access your position!", "NSLocationAlwaysAndWhenInUseUsageDescription": "We need to access your position!", "NSLocationAlwaysUsageDescription": "We need to access your position!"}} }}';
+    localizationStringsJSON = '{"languages":["pt","it","en"],"translations":{"pt":{"camera":"Precisamos aceder a tua câmera!", "location":"Precisamos da tua localização!"}, "it":{"camera":"Dobbiamo accedere alla tua fotocamera!", "location":"Dobbiamo accedere alla tua posizione!"}, "en":{"camera":"We need to access your camera!", "location":"We need to access your position!"}}}'
+    //'{"languages":["pt","it", "en"], "translations": {"pt": {"config_ios": {"NSCameraUsageDescription": "Precisamos aceder a tua câmera!", "NSLocationWhenInUseUsageDescription": "Precisamos da tua localização!", "NSLocationAlwaysAndWhenInUseUsageDescription": "Precisamos da tua localização!", "NSLocationAlwaysUsageDescription": "Precisamos da tua localização!"}},"it": {"config_ios": {"NSCameraUsageDescription": "Dobbiamo accedere alla tua fotocamera!", "NSLocationWhenInUseUsageDescription": "Dobbiamo accedere alla tua posizione!", "NSLocationAlwaysAndWhenInUseUsageDescription": "Dobbiamo accedere alla tua posizione!", "NSLocationAlwaysUsageDescription": "Dobbiamo accedere alla tua posizione!"}},"en": {"config_ios": {"NSCameraUsageDescription": "We need to access your camera!", "NSLocationWhenInUseUsageDescription": "We need to access your position!", "NSLocationAlwaysAndWhenInUseUsageDescription": "We need to access your position!", "NSLocationAlwaysUsageDescription": "We need to access your position!"}} }}';
 
 
     //{ "config_ios": {"NSCameraUsageDescription": "Take pictures", "NSLocationWhenInUseUsageDescription": "Hey I want to track you!", "NSLocationAlwaysAndWhenInUseUsageDescription": "Hey I want to track you!", "NSLocationAlwaysUsageDescription": "Hey I want to track you!"}}
@@ -40,16 +41,66 @@ module.exports = function(context) {
         fs.mkdirSync(translationsDir, { recursive: true });
     }
 
+    //Read Json template
+    var jsonTemplatePath = path.join(context.opts.projectRoot, 'plugins/cordova-plugin-localization-strings/scripts/language_template.js');
+    var jsonTemplate;
+    fs.readFile(jsonTemplatePath, function read(err, data) {
+      if (err) {
+          console.log("🚨 Error reading json template file");
+          throw err;
+      }
+      jsonTemplate = data;
+    });
+
+
+    console.log("👉 " + jsonTemplate);
+
+/*
+
+    fs.readFile(someFile, 'utf8', function (err,data) {
+      if (err) {
+        return console.log("🚨 Error reading json template file: " + err);
+      }
+      var result = data.replace(/string to be replaced/g, 'replacement');
+
+      fs.writeFile(someFile, result, 'utf8', function (err) {
+         if (err) return console.log(err);
+      });
+    });
+
+
+
+
+    var jsonTemplatePath = path.join(context.opts.projectRoot, 'plugins/cordova-plugin-localization-strings/scripts/language_template.js');
+    const {readFile, writeFile, promises: fsPromises} = require('fs');
+    readFile(jsonTemplatePath, 'utf-8', function (err, contents) {
+      if (err) {
+        console.log("🚨 Error reading json template file: " + err);
+        return;
+      }
+
+      const replaced = contents.replace(/to be replaced/g, 'replacement string');
+
+      writeFile('./example.txt', replaced, 'utf-8', function (err) {
+        console.log(err);
+      });
+    });
+
+*/
+
     //Checks required languages array and create respective files
     for (let i = 0; i < localizations.languages.length; i++) {
       let translationFilePath = path.join(translationsDir, localizations.languages[i] + ".json");
       let translation = localizations.translations[localizations.languages[i]];
 
-      fs.writeFile(translationFilePath, translation, function(err) {
+      var result = jsonTemplate.replace(/CAMERA_MESSAGE_PLACEHOLDER/g, translation.camera);
+      result = jsonTemplate.replace(/LOCATION_MESSAGE_PLACEHOLDER/g, translation.location);
+
+      fs.writeFile(translationFilePath, result, function(err) {
         if(err) {
             return console.log("🚨 Error writing translation file " + localizations.languages[i] + ".json: " + err);
         }
-        console.log("The file was saved!");
+        console.log("👍 The file was saved!");
       }); 
     }
 /*
